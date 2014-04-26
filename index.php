aadffb8cd2836b5d99af414bbc513c4b410d830a
Charles <carlos.darth@gmail.com>
2014-04-25 21:30:54 -0700

    Iniciamos :D!

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php	
+
+for ($i=0; $i < 10; $i++) { 
+	echo "tus nalguitas";
+}
+
+echo "qué pedo a ver si con codigo se puede";
+echo "qué más cambios le quieres hacer?";
+echo "a ver pon un boton o algo, ok?";
+
+echo "la neta no me acuerdo haha";
+
+echo 1+5;
+echo "tu culo" * 4;
+
+?>
